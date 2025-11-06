38ba10873d793675db68e5a3f48b4f888a196642
renes377 <renes77@freenet.de>
2025-11-06 18:40:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Service Worker (sw.js) - Minimalversion
+// Dieser Worker ist absichtlich einfach gehalten.
+self.addEventListener('fetch', function(event) {
+  // Er dient hauptsächlich dazu, die PWA-Installation zu triggern.
+});
